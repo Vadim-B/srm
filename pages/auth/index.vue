ed899bcc021a9d5b524ee0d5beac5fa3a8197baf
Vadim-B <boichev88@gmail.com>
2020-11-03 21:17:07 +0200

    fix

--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -5,31 +5,33 @@
            :class="{ 'slide-up': authType === 'login' ||  authType === 'forgotPass' }"
       >
         <h2 class="form-title" id="signup" @click="showSignup"><span>or</span>Sign up</h2>
-
         <div class="form-holder">
           <input type="text" class="input" placeholder="Name" />
           <input type="email" class="input" placeholder="Email" />
           <input type="password" class="input" placeholder="Password" />
         </div>
-
         <button class="submit-btn">Sign up</button>
       </div>
-
-      <div class="login"
-           :class="{ 'slide-up': authType === 'signup' }"
+      <div class="login" :class="{ 'slide-up': authType === 'signup' }"
       >
-        <div class="center">
-
-          <h2 class="form-title" id="login" @click="showLogin"><span>or</span>Log in</h2>
-
+        <div id="login" class="center">
+          <h2 class="form-title" @click="showLogin"><span>or</span>Log in</h2>
           <div class="form-holder">
             <input type="email" class="input" placeholder="Email" />
             <input type="password" class="input" placeholder="Password" />
           </div>
-
           <button class="submit-btn">Log in</button>
+          <p @click="authType = 'forgotPass'">Напонить пароль</p>
         </div>
 
+<!--        <div id="forgot-password" class="center">-->
+<!--          <h2 class="form-title">Forgot password</h2>-->
+<!--          <div class="form-holder">-->
+<!--            <input type="email" class="input" placeholder="Email" />-->
+<!--          </div>-->
+<!--          <button class="submit-btn">Forgot</button>-->
+<!--          <p @click="authType = 'login'">Вернуться на логин</p>-->
+<!--        </div>-->
       </div>
     </div>
   </main>
